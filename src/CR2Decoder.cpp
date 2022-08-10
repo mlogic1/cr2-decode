63e3cc2773199a6ecd982c778383ead77ec353dd
mlogic1 <multilogic@outlook.com>
2022-08-10 19:29:50 +0200

    updated how header is read

--- src/CR2Decoder.cpp
+++ src/CR2Decoder.cpp
@@ -1,4 +1,4 @@
-#include "CR2Header.h"
+#include "CR2ImageData.h"
 #include "Constants.h"
 #include <CR2Decoder.h>
 #include <CR2File.h>
@@ -8,24 +8,16 @@
 #include <stdio.h>
 
 /* private API */
-static CR2Header _C2DDecodeHeader(char* headerBin)
+static void _C2DDecodeHeader(CR2Header* header, std::ifstream& fStream)
 {
-	CR2Header header;
-	memcpy(header.ByteOrdering, headerBin, 2);
-	memcpy(&header.TiffMagicWord, headerBin + 2, 2);
-	memcpy(&header.TiffOffset, headerBin + 4, 4);
-	memcpy(&header.CR2MagicWord, headerBin + 8, 2);			// this one is copied in the reverse order, its 0x5243 - check is it the same on windows and fix
-	header.CR2VerMajor = headerBin[10];
-	header.CR2VerMinor = headerBin[11];
-	memcpy(&header.RawIFDOffset, headerBin + 12, sizeof(long));
-
-	printf("0x%04x\n", header.CR2MagicWord);
-
-	std::cout << "byte 8 = " << headerBin[8] << std::endl;
-	std::cout << "byte 9 = " << headerBin[9] << std::endl;
-	printf("%d\n", header.RawIFDOffset);
-
-	return header;
+	size_t HDDR_SIZE = sizeof(CR2Header);
+	fStream.read((char*)&header->ByteOrdering, 2);
+	fStream.read((char*)&header->TiffMagicWord, 2);
+	fStream.read((char*)&header->TiffOffset, 4);
+	fStream.read((char*)&header->CR2MagicWord, 2);	// this one is copied in the reverse order, its 0x5243 - check is it the same on windows and fix
+	fStream.read((char*)&header->CR2VerMajor, 1);
+	fStream.read((char*)&header->CR2VerMinor, 1);
+	fStream.read((char*)&header->RawIFDOffset, 4);
 }
 
 CR2File* C2DLoad(const char* path)
@@ -35,19 +27,11 @@ CR2File* C2DLoad(const char* path)
 		return nullptr;
 	}
 
-	char* headerBin = new char[HEADER_SIZE];
-	inStream.read(headerBin, HEADER_SIZE);
-	CR2Header header = _C2DDecodeHeader(headerBin);
-
-
+	CR2ImageData* imData = new CR2ImageData();
+	_C2DDecodeHeader(&imData->Header, inStream);
 	// read the rest of the content
-	// inStream.read(headerBin, HEADER_SIZE);
-
-	delete headerBin;
 
 	CR2File* f = new CR2File();
-
-
 	return f;
 }
 
---
+++ src/CR2ImageData.h
@@ -0,0 +1,11 @@
+#ifndef CR2_IMAGE_DATA_H
+#define CR2_IMAGE_DATA_H
+
+#include "CR2Header.h"
+
+struct CR2ImageData
+{
+	CR2Header Header;
+};
+
+#endif // CR2_IMAGE_DATA_H
