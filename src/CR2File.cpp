b2128e2968f960b8a0bfdca9015806cc284a4432
mlogic1 <multilogic@outlook.com>
2024-02-11 11:30:29 +0100

    fixed memory leak in jpeg preview buffer

--- src/CR2File.cpp
+++ src/CR2File.cpp
@@ -11,7 +11,7 @@ void CR2FileExtractPreviewImage(CR2File* file, const std::string& outputPath)
 	}
 	const CR2IDFFrame* IDF0 = file->ImageData->Frames.at(0);
 	std::ifstream* fStream = file->InStream;
-	
+
 	char* jpegBuffer = new char[IDF0->StripByteCounts];
 	const std::streampos curPos = fStream->tellg();
 	fStream->seekg(IDF0->StripOffsets[0]);
@@ -19,4 +19,5 @@ void CR2FileExtractPreviewImage(CR2File* file, const std::string& outputPath)
 	std::ofstream jpegPreview(outputPath, std::ios::out | std::ios::binary);
 	jpegPreview.write(jpegBuffer, IDF0->StripByteCounts);
 	jpegPreview.close();
+	delete[] jpegBuffer;
 }
